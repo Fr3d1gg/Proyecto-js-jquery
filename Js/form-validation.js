0dbb0ed0d505d698ccffa1b77dbbb22ec456c9a8
Fredy <josealfredoramirezaguirre113@gmail.com>
2024-08-21 23:03:47 -0600

    validacion de un formulario

---
+++ Js/form-validation.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    console.log("Cargado correctamente");
+
+    // Datepicker en el input
+    $("#datepicker").datepicker();
+
+   
+    // Inicializar la validación del formulario
+    $.validate({
+        lang:'es'
+    });
+
+});
